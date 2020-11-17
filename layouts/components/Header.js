25f010f6e55618c2ec398468c1cca18a33edd222
tampm <phanminhtam1992@gmail.com>
2020-11-17 13:45:34 +0100

    update fix

--- layouts/components/Header.js
+++ layouts/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import TpLogo from '@/layouts/components/TpLogo'
+import Logo from '@/layouts/components/Logo'
 import Social from '@/layouts/components/Social'
 
 const Header = () => (
   <div className="w-full container mx-auto p-6">
     <div className="flex justify-between items-center">
-      <TpLogo />
+      <Logo />
       <Social />
 
     </div>
